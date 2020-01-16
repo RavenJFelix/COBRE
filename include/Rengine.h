ec25e591901512f6352258a53791f01669fb6bad
Raven Jyro Felix <raven@localhost.localdomain>
2020-01-16 14:37:31 -0500

    Implement basic Rengine interface

--- include/Rengine.h
+++ include/Rengine.h
@@ -1,17 +1,26 @@
 #ifndef RENGINE_H
 #define RENGINE_H
 
+#include "Canvas.h"
 #include "Pixel.h"
+#include "PixelBuffer.h"
 
 #include <vector>
 
 namespace cobre
 {
-	class Rengine
-	{
-	private:
-		std::vector<Pixel> m_pixBuffer;
-	};
+    namespace core
+    {
+        class Rengine
+        {
+            private:
+                std::vector<Pixel> m_pixBuffer;
+            public:
+                Rengine(){}
+
+
+        };
+    }
 }	
 
 #endif
